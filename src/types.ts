d9c4f424aef55846250ce7505cf74037ce74812f
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-09-25 23:58:50 -0300

    feat: type structural PDF cleanup option

--- src/types.ts
+++ src/types.ts
@@ -186,6 +186,7 @@ export interface SanitizeOptions {
   removeAnnotations: boolean;
   removeForms: boolean;
   removeMultimedia: boolean;
+  cleanupStructure: boolean;
 }
 
 export interface SanitizeReport {
